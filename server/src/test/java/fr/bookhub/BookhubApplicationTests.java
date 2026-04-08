ecc4727b990d0ec10c84dacedf0fdab2bad7dd75
Océane <ocebrs@gmail.com>
2026-04-08 14:55:10 +0200

    build: initial project structure

---
+++ server/src/test/java/fr/bookhub/BookhubApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bookhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
