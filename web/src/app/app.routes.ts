2fdeedca5c3c6caecbb4f89d01323a58247a11e5
aflorent44 <amelie.florent2024@campus-eni.fr>
2026-04-09 16:14:42 +0200

    feat: add book form

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'book', loadComponent: () => import('./features/book/book-form/book-form').then(m => m.BookForm), title: 'Ajouter un livre' },
+];
