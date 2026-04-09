c449671621fedce22b20f10f6089d16950a7b8b0
Océane <ocebrs@gmail.com>
2026-04-09 15:56:22 +0200

    feat(user): add registration form frontend

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,11 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    loadComponent: () => import('./features/user/registration-form/registration-form')
+      .then(m => m.RegistrationForm),
+    title: 'Inscription - Bookhub'
+  },
+
+];
