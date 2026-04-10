4fc3a50f7b155334fd81f19d28e22680c1509451
aflorent44 <amelie.florent2024@campus-eni.fr>
2026-04-10 12:33:26 +0200

    feat: add book detail component

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: 'book', loadComponent: () => import('./features/book/book-form/book-form').then(m => m.BookForm), title: 'Ajouter un livre' },
 
+  { path: 'book-detail/:id', loadComponent: () => import('./features/book/book-detail/book-detail').then(m => m.BookDetail), title: 'Afficher un livre' },
+
   {  path: 'register',
      loadComponent: () => import('./features/user/registration-form/registration-form')
       .then(m => m.RegistrationForm),
--- web/src/app/core/types/author.ts
+++
@@ -1,9 +0,0 @@
-export interface Author {
-  id: number;
-  firstName: string;
-  lastName: string;
-  createdAt: Date;
-  updatedAt: Date;
-  createdBy: number;
-  updatedBy: number;
-}
