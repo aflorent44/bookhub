fda8ddbe1b61f487b9e6eddecd30c13b8ea917c5
AxelENI <axel.vincent2025@campus-eni.fr>
2026-04-14 09:56:37 +0200

    fix(10-recherche-et-filtres): delete System.out.prinln

--- server/src/main/java/fr/bookhub/service/AuthorService.java
+++ server/src/main/java/fr/bookhub/service/AuthorService.java
@@ -22,22 +22,9 @@ public class AuthorService {
         if (req.getLastName().isEmpty()) return new ServiceResponse<>("3002", "Last name is empty");
         if (req.getCountry() == null) return new ServiceResponse<>("3003", "Country is empty");
 
-        System.out.println("Reçu: [" + req.getCountry() + "]");
-        System.out.println("Longueur: " + req.getCountry().length());
-        System.out.println("Bytes: " + java.util.Arrays.toString(req.getCountry().getBytes()));
-
         Author author = new Author();
         author.setFirstName(req.getFirstName());
         author.setLastName(req.getLastName());
-
-        Optional<Country> country = countryRepository.findByNameIgnoreCase(req.getCountry());
-
-        if (country.isPresent()) {
-            System.out.println("Country existente" + country.get());
-            author.setCountry(country.get());
-        } else {
-            System.out.println("Country non existente");
-        }
 
         author.setCountry(
                 countryRepository.findByNameIgnoreCase(req.getCountry())
